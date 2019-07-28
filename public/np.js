2d8f001e59a1976a419b582ae7164f80d965d8ad
zenlykoi <nguyenphuong.2661999@gmail.com>
2019-07-28 15:29:06 +0700

    add np-if DOM and comment all code

--- public/np.js
+++ public/np.js
@@ -1,10 +1,60 @@
+/*
+ * @name : NPjs
+ * @description : The simple single page app
+ * @author : Nguyen Phuong(NP)
+ * @version : 0.1.0
+ */
 let NP = {
+    /*
+     * @name : initId
+     * @type : variable(string)
+     * @description : DOM id to init NPjs
+     */
     initId: "",
+    /*
+     * @name : routes
+     * @type : variable(array)
+     * @description : Array to store all page router
+     * @struct : [
+     *              {
+     *                  path : 'Url page',
+     *                  template : {
+     *                      title : 'Title of page',
+     *                      html : `HTML of page (multiline string)`
+     *                  } 
+     *              },...
+     *          ]
+     */
     routes: [],
+    /*
+     * @name : data
+     * @type : variable(object)
+     * @description : Object to store all NP data
+     * @struct : {
+     *              if : {
+     *                  example : true(or false)  -> if DOM
+     *              },...
+     *           }
+     */
+    data: {},
+    /*
+     * @name : attrIf
+     * @type : variable(string)
+     * @description : The name of attribute if DOM
+     */
+    attrIf: 'np-if',
+    /*
+     * @name : initTemplateByRouter
+     * @author : Nguyen Phuong(NP)
+     * @type : function
+     * @functional : 
+     *      - Init HTML of router to DOM(initId)
+     *      - Delete all DOM have attribute np-if == false
+     */
     initTemplateByRouter: function() {
         let routesLength = this.routes.length;
         let path = (location.pathname[location.pathname.length-1] == '/') ? location.pathname.slice(0,location.pathname.length-1) : location.pathname;
-        let template;
+        let template,listIfDOM,listIfDOMLength;
         if (routesLength > 0){
             for (let i = 0; i< routesLength; i++) {
                 if(path == this.routes[i].path){
@@ -14,9 +64,25 @@ let NP = {
             }
             document.title = template.title;
             document.getElementById(this.initId).innerHTML = template.html;
+            listIfDOM = this.getAllElementsByAttr(this.attrIf);
+            listIfDOMLength = listIfDOM.length;
+            for(let i=0; i<listIfDOMLength; i++){
+                if(this.data.if[listIfDOM[i].getAttribute(this.attrIf)] == false){
+                    document.getElementById(this.initId).removeChild(listIfDOM[i]);
+                }
+            }
             this.initEventToClickRoute();
         }
     },
+    /*
+     * @name : initEventToClickRoute
+     * @author : Nguyen Phuong(NP)
+     * @type : function
+     * @functional : 
+     *      - Init onclick event to all tag 'a' of DOM and disable attr href of this tag
+     *      - When click tag 'a',NP will run history.pushState() function with 'a' href
+     *      - After that,NP run initTemplateByRouter() function to init HTML
+     */
     initEventToClickRoute: function() {
         let that = this;
         let routes = document.getElementsByTagName("a");
@@ -33,6 +99,13 @@ let NP = {
             }
         }
     },
+    /*
+     * @name : renderAfterLoad
+     * @author : Nguyen Phuong(NP)
+     * @type : function
+     * @functional : 
+     *      - Active initEventToClickRoute() function and initTemplateByRouter() function in some cases
+     */
     renderAfterLoad: function() {
         let that = this;
         that.initEventToClickRoute();
@@ -47,9 +120,27 @@ let NP = {
             that.initTemplateByRouter();
         })
     },
+    /*
+     * @name : getAllElementsByAttr
+     * @author : Nguyen Phuong(NP)
+     * @type : function(support)
+     * @params : 
+     *      - attrib : Attribute of DOM element you want to get
+     * @functional : 
+     *      - Get all element have someone attribute
+     */
     getAllElementsByAttr: function(attrib){
         return document.querySelectorAll('['+attrib+']');
     },
+    /*
+     * @name : init
+     * @author : Nguyen Phuong(NP)
+     * @type : function
+     * @params : 
+     *      - id : The attribute 'id' of DOM element you want to init NPjs
+     * @functional : 
+     *      - Init NPjs
+     */
     init: function(id) {
         this.initId = id;
         this.renderAfterLoad();
